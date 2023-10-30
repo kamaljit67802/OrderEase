9209548fee3b33c4cc01ef8bbecab318a9911f62
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2023-10-30 09:21:52 +0530

    Add project files.

---
+++ OrderEase/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderEase
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OrderEase/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace YOrderApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            // Implement login logic here
+        }
+
+        private void OnMenuClicked(object sender, EventArgs e)
+        {
+            // Navigate to the menu page
+            // Implement navigation logic here
+        }
+
+        private void OnTrackOrderClicked(object sender, EventArgs e)
+        {
+            // Navigate to the order tracking page
+            // Implement navigation logic here
+        }
+    }
+}
